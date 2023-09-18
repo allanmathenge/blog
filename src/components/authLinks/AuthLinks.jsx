9a787ed6d9005443abb5b70f4970f14f1f697010
allan <allanmathenge2@gmail.com>
2023-09-18 14:02:31 +0300

    first commit

---
+++ src/components/authLinks/AuthLinks.jsx
@@ -0,0 +1,31 @@
+"use client"
+
+import React, { useState } from 'react'
+import Link from "next/link"
+import styles from "./authLinks.module.css"
+
+const AuthLinks = () => {
+
+  const [open, setOpen] = useState(false)
+    //temporary
+    const status = "notauthenticated"
+  return (
+    <>
+    {status === "notauthenticated" ? (
+        <Link href="/login">Login</Link>
+    ) : (
+        <>
+        <Link href="/write">Write</Link>
+        <span className={styles.link}>Logout</span>
+        </>
+      )}
+      <div className={styles.burger}>
+        <div className={styles.line}></div>
+        <div className={styles.line}></div>
+        <div className={styles.line}></div>
+      </div>
+    </>
+  )
+}
+
+export default AuthLinks
